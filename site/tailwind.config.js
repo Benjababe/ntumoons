525487212c424e0e79d0d2cb005eb4b0ed304b5d
Benjababe <bengohzy@gmail.com>
2023-12-27 15:55:21 +0800

    feet: ran prettier

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -6,66 +6,64 @@ export default {
     theme: {
         extend: {
             aspectRatio: {
-                'staff-photo': '3 / 4',
+                'staff-photo': '3 / 4'
             },
             backgroundImage: {
                 'tt-alternate-h': 'linear-gradient(90deg, var(--tt-alternate) 50%, transparent 0)',
                 'tt-alternate-v': 'linear-gradient(360deg, var(--tt-alternate) 50%, transparent 0)'
             },
             backgroundSize: {
-                'tt-loop': '16.7% 16.7%',
+                'tt-loop': '16.7% 16.7%'
             },
             borderWidth: {
-                '3': '3px',
+                3: '3px'
             },
             gridTemplateColumns: {
-                'info': '2fr 5fr',
+                info: '2fr 5fr'
             },
             minWidth: {
-                '4/5': '80%',
+                '4/5': '80%'
             },
             maxWidth: {
-                '1200': '1200px',
+                1200: '1200px'
             },
             scale: {
-                '120': '1.2',
+                120: '1.2'
             }
-        },
+        }
     },
     daisyui: {
         themes: [
             {
                 light: {
-                    "color-scheme": "light",
-                    "primary": "#3b82f6",
-                    "secondary": "oklch(69.71% 0.329 342.55)",
-                    "secondary-content": "oklch(98.71% 0.0106 342.55)",
-                    "accent": "#2563eb",
-                    "neutral": "#9ca3af",
-                    "neutral-content": "#D7DDE4",
-                    "base-100": "oklch(100% 0 0)",
-                    "base-200": "#F2F2F2",
-                    "base-300": "#E5E6E6",
-                    "base-content": "#1f2937",
-                    '--tt-alternate': '#dddddd',
+                    'color-scheme': 'light',
+                    primary: '#3b82f6',
+                    secondary: 'oklch(69.71% 0.329 342.55)',
+                    'secondary-content': 'oklch(98.71% 0.0106 342.55)',
+                    accent: '#2563eb',
+                    neutral: '#9ca3af',
+                    'neutral-content': '#D7DDE4',
+                    'base-100': 'oklch(100% 0 0)',
+                    'base-200': '#F2F2F2',
+                    'base-300': '#E5E6E6',
+                    'base-content': '#1f2937',
+                    '--tt-alternate': '#dddddd'
                 },
                 dark: {
-                    "color-scheme": "dark",
-                    "primary": "#3b82f6",
-                    "secondary": "#99f6e4",
-                    "accent": "#2563eb",
-                    "neutral": "#e5e7eb",
-                    "base-100": "#1f2937",
-                    "info": "#3b82f6",
-                    "success": "#4ade80",
-                    "warning": "#fbbf24",
-                    "error": "#ef4444",
-                    '--tt-alternate': '#1a1e2c',
+                    'color-scheme': 'dark',
+                    primary: '#3b82f6',
+                    secondary: '#99f6e4',
+                    accent: '#2563eb',
+                    neutral: '#e5e7eb',
+                    'base-100': '#1f2937',
+                    info: '#3b82f6',
+                    success: '#4ade80',
+                    warning: '#fbbf24',
+                    error: '#ef4444',
+                    '--tt-alternate': '#1a1e2c'
                 }
-            }],
+            }
+        ]
     },
-    plugins: [
-        daisyui,
-    ],
-}
-
+    plugins: [daisyui]
+};
