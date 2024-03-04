27c71a9a735f31133980c9a4ae3d5c2449a17ca1
Benjababe <bengohzy@gmail.com>
2024-03-04 11:59:34 +0800

    feet: include modal component

---
+++ site/src/lib/components/generic/Modal.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let dialog: HTMLDialogElement;
+
+    function showModal() {
+        dialog.showModal();
+    }
+</script>
+
+<button on:click={showModal}>
+    <slot name="modal-button" />
+</button>
+
+<dialog
+    class="modal modal-bottom sm:modal-middle"
+    bind:this={dialog}
+>
+    <div class="modal-box">
+        <slot name="modal-body" />
+    </div>
+    <form
+        method="dialog"
+        class="modal-backdrop"
+    >
+        <button class="cursor-default"></button>
+    </form>
+</dialog>
