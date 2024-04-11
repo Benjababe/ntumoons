85e651364b89ed871bf325b5c9136497fa44110f
Benjababe <bengohzy@gmail.com>
2024-04-11 11:32:03 +0800

    Update rate limiter (#85)
    * chore: update dependencies
    
    * chore: update packages
    
    * chore: update package.json
    
    * fix: package.json
    
    * chore: package-lock update
    
    * chore: remove package-lock, migrate to pnpm
    
    * chore: update eslint plugins
    
    * chore: update linter workflow for pnpm
    
    * chore: update linter workflow
    
    * chore: rewrote linter workflow file
    
    * feet: add check for admin route
    
    * fix: temporarily disable rate limit
    
    * chore: remove in memory rate limit
    
    * feet: use upstash for rate limiting (vercel KV)

--- site/src/hooks.server.ts
+++ site/src/hooks.server.ts
@@ -1,29 +1,24 @@
-import { SECRET_ADMIN_KEY } from '$env/static/private';
+import { SECRET_ADMIN_KEY, KV_REST_API_URL, KV_REST_API_TOKEN } from '$env/static/private';
 import { error } from '@sveltejs/kit';
-import { RetryAfterRateLimiter } from 'sveltekit-rate-limiter/server';
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
 
-const rl = new RetryAfterRateLimiter({
-    IP: [20, 'h'],
-    IPUA: [10, 'm']
+const rl = new Ratelimit({
+    redis: new Redis({ url: KV_REST_API_URL, token: KV_REST_API_TOKEN }),
+    limiter: Ratelimit.slidingWindow(10, '1 m'),
+    prefix: '@upstash/ratelimit'
 });
 
 export const handle = async ({ event, resolve }) => {
-    const status = await rl.check(event);
-    if (status.limited) {
-        const response = new Response(
-            `You are being rate limited. Please try after ${status.retryAfter} seconds.`,
-            {
-                status: 429,
-                headers: { 'Retry-After': status.retryAfter.toString() }
-            }
-        );
-        return response;
-    }
-
     const reqPath = event.url.pathname;
 
-    if (reqPath.startsWith('/admin') && event.url.searchParams.get('token') !== SECRET_ADMIN_KEY)
-        return error(403, { message: 'You are unauthorised to access this page' });
+    if (reqPath.startsWith('/admin')) {
+        const { success } = await rl.limit(event.getClientAddress());
+        if (!success) return error(429, { message: 'Too many requests being sent recently' });
+
+        if (event.url.searchParams.get('token') !== SECRET_ADMIN_KEY)
+            return error(403, { message: 'You are unauthorised to access this page' });
+    }
 
     return await resolve(event);
 };
